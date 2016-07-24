4f341b1386229d28fb1e355944d4aca467d383a4
alirezaSoltani <alireza.soltani1373@gmail.com>
2016-07-24 13:57:28 +0430

    Login

--- Second/Second/Form1.Designer.cs
+++ Second/Second/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Second
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,50 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.login_user_txt = new System.Windows.Forms.Label();
+            this.login_pass_txt = new System.Windows.Forms.Label();
+            this.login_user_tb = new System.Windows.Forms.TextBox();
+            this.login_pass_tb = new System.Windows.Forms.TextBox();
+            this.login_enter_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // login_user_txt
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(471, 168);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "علیرضا";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.login_user_txt.AutoSize = true;
+            this.login_user_txt.Location = new System.Drawing.Point(414, 175);
+            this.login_user_txt.Name = "login_user_txt";
+            this.login_user_txt.Size = new System.Drawing.Size(57, 13);
+            this.login_user_txt.TabIndex = 0;
+            this.login_user_txt.Text = "کد کاربری";
             // 
-            // button1
+            // login_pass_txt
             // 
-            this.button1.Location = new System.Drawing.Point(29, 18);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "hosein";
-            this.button1.UseVisualStyleBackColor = true;
+            this.login_pass_txt.AutoSize = true;
+            this.login_pass_txt.Location = new System.Drawing.Point(424, 221);
+            this.login_pass_txt.Name = "login_pass_txt";
+            this.login_pass_txt.Size = new System.Drawing.Size(47, 13);
+            this.login_pass_txt.TabIndex = 1;
+            this.login_pass_txt.Text = "رمز عبور";
             // 
-            // button2
+            // login_user_tb
             // 
-            this.button2.Location = new System.Drawing.Point(825, 35);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Nima";
-            this.button2.UseVisualStyleBackColor = true;
+            this.login_user_tb.Location = new System.Drawing.Point(218, 175);
+            this.login_user_tb.Name = "login_user_tb";
+            this.login_user_tb.Size = new System.Drawing.Size(146, 20);
+            this.login_user_tb.TabIndex = 2;
             // 
-            // Form1
+            // login_pass_tb
+            // 
+            this.login_pass_tb.Location = new System.Drawing.Point(218, 221);
+            this.login_pass_tb.Name = "login_pass_tb";
+            this.login_pass_tb.Size = new System.Drawing.Size(146, 20);
+            this.login_pass_tb.TabIndex = 3;
+            // 
+            // login_enter_btn
+            // 
+            this.login_enter_btn.Location = new System.Drawing.Point(251, 268);
+            this.login_enter_btn.Name = "login_enter_btn";
+            this.login_enter_btn.Size = new System.Drawing.Size(75, 23);
+            this.login_enter_btn.TabIndex = 4;
+            this.login_enter_btn.Text = "ورود";
+            this.login_enter_btn.UseVisualStyleBackColor = true;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1016, 596);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(543, 439);
+            this.Controls.Add(this.login_enter_btn);
+            this.Controls.Add(this.login_pass_tb);
+            this.Controls.Add(this.login_user_tb);
+            this.Controls.Add(this.login_pass_txt);
+            this.Controls.Add(this.login_user_txt);
+            this.Name = "Login";
+            this.Text = "login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,9 +95,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label login_user_txt;
+        private System.Windows.Forms.Label login_pass_txt;
+        private System.Windows.Forms.TextBox login_user_tb;
+        private System.Windows.Forms.TextBox login_pass_tb;
+        private System.Windows.Forms.Button login_enter_btn;
     }
 }
 
