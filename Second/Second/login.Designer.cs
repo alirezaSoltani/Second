e38349dd5d71ce6b3a1587abfb312aaafe1d9d69
engghobeiti <eng.peyman.ghobeyiti@gmail.com>
2016-07-28 15:56:40 +0430

    designe login

--- Second/Second/login.Designer.cs
+++ Second/Second/login.Designer.cs
@@ -31,6 +31,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox2
@@ -65,12 +66,22 @@
             this.label1.Text = "ورود";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(347, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // login1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Second.Properties.Resources.Untitled_232;
             this.ClientSize = new System.Drawing.Size(385, 310);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -79,6 +90,7 @@
             this.Name = "login1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "login";
+            this.Load += new System.EventHandler(this.login1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,5 +101,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Second/Second/login.cs
+++ Second/Second/login.cs
@@ -16,5 +16,15 @@ namespace Second
         {
             InitializeComponent();
         }
+
+        private void login1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
