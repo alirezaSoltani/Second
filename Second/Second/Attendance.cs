e6e6c6e2f77966e50b0488631315921d88029b98
nimakazemi <kazemy.nima1995@gmail.com>
2016-08-11 15:14:55 +0430

    Nima New Commiiiiit2112

--- Second/Second/Attendance.cs
+++ Second/Second/Attendance.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
+using Test;
 namespace Second
 {
     class Attendance
@@ -18,30 +20,86 @@ namespace Second
             lessonGroupNumber = 0;
         }
 
-        public static void attencanceLesson(Attendance attendanceObject)
+        public void attendanceLesson(string date)
         {
 
             try
             {
                 SqlConnection conn = new SqlConnection();
                 conn.ConnectionString =
-                "Data Source=(local);" +
-                "Initial Catalog=Project;" +
-                "Integrated Security=True";
+                  "Data Source= 185.159.152.5;" +
+                    "Initial Catalog=youshita_Test;" +
+                    "User id=youshita_co; " +
+                    "Password=P@hn1395;";
 
-                using (SqlCommand cmd = new SqlCommand("ALTER TABLE [dbo].[" + attendanceObject.lessonNumber + "-" + attendanceObject.lessonGroupNumber + "_Table" + "]"
-                           + "ADD [" + "/*currentDate*/" + "] [INT]", new SqlConnection(conn.ConnectionString)))
+                using (SqlCommand cmd = new SqlCommand("ALTER TABLE [dbo].[" + getLessonNumber() + "-" + getLessonGroupNumber() + "_Table" + "]"
+                           + "ADD [" + date + "] [INT]", new SqlConnection(conn.ConnectionString)))
                 {
                     cmd.Connection.Open();
                     cmd.ExecuteNonQuery();
                     cmd.Connection.Close();
                 }
+
             }
-            catch (Exception)
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+        public void attend(String date,bool state)
+        {
+            Attendance attendanceObj = new Attendance();
+            StudentModel studentObj = new StudentModel();
+            try
             {
 
-                throw;
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString =
+                  "Data Source= 185.159.152.5;" +
+                    "Initial Catalog=youshita_Test;" +
+                    "User id=youshita_co; " +
+                    "Password=P@hn1395;";
+
+                SqlCommand sc = new SqlCommand();
+                SqlDataReader reader;
+                sc.CommandText = "UPDATE [dbo].[" + getLessonNumber() + "-" + getLessonGroupNumber() + "_Table" + "] SET [" + date + "] = '" + state + "' WHERE student# = " + studentObj.getStudentNumber() + "";
+               
+
+                sc.CommandType = CommandType.Text;
+                sc.Connection = conn;
+                conn.Open();
+                reader = sc.ExecuteReader();
+                conn.Close();
             }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        public void setLessonNumber(long LessonNumber)
+        {
+            this.lessonNumber = LessonNumber;
+        }
+
+
+        public long getLessonNumber()
+        {
+            return lessonNumber;
+
+        }
+
+        public void setLessonGroupNumber(Int16 LessonGroupNumber)
+        {
+            this.lessonGroupNumber = LessonGroupNumber;
+        }
+
+
+        public Int16 getLessonGroupNumber()
+        {
+            return lessonGroupNumber;
+
         }
     }
 }
